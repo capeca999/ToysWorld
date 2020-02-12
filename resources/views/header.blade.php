b8d9106346f8f45d8fe225e972e386405b33146a
malcacer <47533925+malcacer@users.noreply.github.com>
2020-02-12 21:01:47 +0100

    Estado actual de mi Laravel
    12/02/2020

---
+++ resources/views/header.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body>
+
+<nav class="navbar navbar-light navbar-expand-md custom-header">
+    <div class="container-fluid"><a href="#" class="navbar-brand">
+
+      <img class="logo" src="assets/img/caballo.png">
+        <img class="logo2" src="assets/img/titulo.png">
+<br>
+
+
+
+        <div class="buscar">
+      Buscar Juguetes  <input class="buscartoy" type="text">
+        </div>
+
+    </a><button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div
+            class="collapse navbar-collapse" id="navbar-collapse">
+
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item" role="presentation"><a class="nav-link" href="#"> <img class="iconos" src="assets/img/login.svg"> LogIn </a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="#"> <img class="iconos" src="assets/img/register.svg"> Registrarse </a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="#"> <img class="iconos" src="assets/img/basket.svg"> Carrito</a></li>
+                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><img src="assets/img/avatar.jpg" class="dropdown-image" /></a>
+                    <div class="dropdown-menu dropdown-menu-right" role="menu"><a class="dropdown-item" role="presentation" href="#">Perfil </a><a class="dropdown-item" role="presentation" href="#">Pedidos </a><a class="dropdown-item" role="presentation" href="#">Logout </a></div>
+                </li>
+            </ul>
+    </div>
+    </div>
+</nav>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
