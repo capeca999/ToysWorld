8c0e7f75dc18e124c2bdaffb4dce9781631fd1e9
malcacer <47533925+malcacer@users.noreply.github.com>
2020-02-13 17:01:12 +0100

    Layout master terminado
    Routes actualizado
    Layour master terminado
    Imagenes y css movido a public

--- resources/views/cesta.blade.php
+++ resources/views/cesta.blade.php
@@ -1,47 +1,51 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>ToysWorld</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/stylecesta.css" type="text/css">
-</head>
-<body>
-    <?php include 'header.blade.php'?>
-    <main class="row">
-        <section id="izquierda" class="col-md-9 p-5">
-            <article class="col-md-12 pb-2"><form action="">
-                <span class="grande">Patinete Xiaomi</span><br>
-                <span class="medio">Patinete muy rápido y bien barato amigo lleno de dineros y de cositas las vainvas van como vainas vainas de vaineo le das al klkeo le das bien duro al kelokeo213123jasjdjasdj addsadsasd asdasdasdasd asdasdasd asdasdasdasdasd asdasdasd</span><br>
-                <span class="font-weight-bold medio">Precio: 25€</span><span class="font-weight-bold ml-5 medio">Cantidad: </span><input type="number" value="2"><span class="font-weight-bold ml-5 medio"></span><span class="font-weight-bold ml-5 medio">Precio total:</span></form>  
-            </article>
-               
-            <article class="col-md-12 pb-2"><form action="">
-                <span class="grande">Yoyo supersónico</span><br>
-                <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
-                <span class="font-weight-bold medio">Precio: 800€</span><span class="font-weight-bold ml-5 medio">Cantidad: </span><input type="number" value="2"><span class="font-weight-bold ml-5 medio"></span><span class="font-weight-bold ml-5 medio">Precio total:</span></form>  
-            </article>
-               
-            <article class="col-md-12 pb-2"><form action="">
-                <span class="grande">Vaina loca</span><br>
-                <span class="medio">UNA VAINA LOCA QUE ME LLEVA A LA GLORRIAA NUNCA HE SENTIDO RARATATATAAAA</span><br>
-                <span class="font-weight-bold medio">Precio: 56€</span><span class="font-weight-bold ml-5 medio">Cantidad: </span><input type="number" value="2"><span class="font-weight-bold ml-5 medio"></span><span class="font-weight-bold ml-5 medio">Precio total:</span></form>  
-            </article>
-            
-        </section>
+@extends('layouts.master')
+@section('contenido')
+<main class="row">
+    <section id="izquierda" class="col-md-9 p-5">
+        <article class="col-md-12 pb-2"><form action="">
+            <span class="grande">Yoyo supersónico</span><br>
+            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
+            <div class="row">
+                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
+                <div class="font-weight-bold col-md-4 medio">Cantidad: 1</div>
+                <div class="font-weight-bold col-md-4 medio">Precio total: 800€</div>
+            </div>
+            </form>  
+        </article>
         
-        <section id="derecha" class="col-md-3">
-            <article class="m-5">
-                <table id="tabla">
-                    <tr><td class="grande">Resumen de pedido</td></tr>
-                    <tr><td>ss</td></tr>
-                    <tr><td>ss</td></tr>
-                    <tr><td>ss</td></tr>
-                    <tr><td>ss</td></tr>
-                </table>
-            </article>
-        </section>
+        <article class="col-md-12 pb-2"><form action="">
+            <span class="grande">Yoyo supersónico</span><br>
+            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
+            <div class="row">
+                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
+                <div class="font-weight-bold col-md-4 medio">Cantidad: 4</div>
+                <div class="font-weight-bold col-md-4 medio">Precio total: 3200€</div>
+            </div>
+            </form>  
+        </article>
         
-    </main>
-</body>
-</html>
+        <article class="col-md-12 pb-2"><form action="">
+            <span class="grande">Yoyo supersónico</span><br>
+            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
+            <div class="row">
+                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
+                <div class="font-weight-bold col-md-4 medio">Cantidad: 3</div>
+                <div class="font-weight-bold col-md-4 medio">Precio total: 2400€</div>
+            </div>
+            </form>  
+        </article>
+    </section>
+    
+    <section id="derecha" class="col-md-3">
+        <article class="m-5">
+            <table id="tabla">
+                <tr><td class="grande">Resumen de pedido</td></tr>
+                <tr><td>ss</td></tr>
+                <tr><td>ss</td></tr>
+                <tr><td>ss</td></tr>
+                <tr><td>ss</td></tr>
+            </table>
+        </article>
+    </section>
+</main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/cesta', function () {
+    return view('cesta');
 });
