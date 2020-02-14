c79a4a70604636ff47082a377197a325b9e4689c
SandraAP1996 <34686149+SandraAP1996@users.noreply.github.com>
2020-02-14 20:22:14 +0100

    ULTIMA SUBIDAAAA

---
+++ database/migrations/2020_02_10_194502_productos.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Productos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->Integer('stock');
+            $table->bigInteger('impuesto');
+            $table->bigInteger('descuento');
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->string('marca');
+            $table->bigInteger('peso');
+            $table->decimal('precio');
+            $table->bigInteger('edad');
+            $table->enum('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
