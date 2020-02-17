1838905abc9710f08c64c0bc1c67f87d2c6f3c16
Capeca <capeca999@gmail.com>
2020-02-17 20:42:22 +0100

    Añadido CSS Página Principal
    17/02/2020

---
+++ resources/views/cesta.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.master')
+@section('contenido')
+<main class="row">
+    <section id="izquierda" class="col-md-9 p-5">
+        <article class="col-md-12 pb-2"><form action="">
+            <span class="grande">Yoyo supersónico</span><br>
+            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
+            <div class="row">
+                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
+                <div class="font-weight-bold col-md-4 medio">Cantidad: 1</div>
+                <div class="font-weight-bold col-md-4 medio">Precio total: 800€</div>
+            </div>
+            </form>  
+        </article>
+        
+        <article class="col-md-12 pb-2"><form action="">
+            <span class="grande">Yoyo supersónico</span><br>
+            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
+            <div class="row">
+                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
+                <div class="font-weight-bold col-md-4 medio">Cantidad: 4</div>
+                <div class="font-weight-bold col-md-4 medio">Precio total: 3200€</div>
+            </div>
+            </form>  
+        </article>
+        
+        <article class="col-md-12 pb-2"><form action="">
+            <span class="grande">Yoyo supersónico</span><br>
+            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
+            <div class="row">
+                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
+                <div class="font-weight-bold col-md-4 medio">Cantidad: 3</div>
+                <div class="font-weight-bold col-md-4 medio">Precio total: 2400€</div>
+            </div>
+            </form>  
+        </article>
+    </section>
+    
+    <section id="derecha" class="col-md-3">
+        <article class="m-5">
+            <table id="tabla">
+                <tr><td class="grande">Resumen de pedido</td></tr>
+                <tr><td>ss</td></tr>
+                <tr><td>ss</td></tr>
+                <tr><td>ss</td></tr>
+                <tr><td>ss</td></tr>
+            </table>
+        </article>
+    </section>
+</main>
+@endsection
