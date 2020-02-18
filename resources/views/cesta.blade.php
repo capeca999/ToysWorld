b34bfc8be65e8eeaf964fa3547b514ab535eee33
malcacer <47533925+malcacer@users.noreply.github.com>
2020-02-18 14:40:20 +0100

    Cesta y css actualizados
    18/02/2020

--- resources/views/cesta.blade.php
+++ resources/views/cesta.blade.php
@@ -1,51 +1,35 @@
 @extends('layouts.master')
+
+@section('scripts')
+    <script src="/js/cesta.js"></script>
+@endsection
+
+@section('titulo')
+    - Cesta
+@endsection
+
 @section('contenido')
-<main class="row">
-    <section id="izquierda" class="col-md-9 p-5">
-        <article class="col-md-12 pb-2"><form action="">
-            <span class="grande">Yoyo supersónico</span><br>
-            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
-            <div class="row">
-                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
-                <div class="font-weight-bold col-md-4 medio">Cantidad: 1</div>
-                <div class="font-weight-bold col-md-4 medio">Precio total: 800€</div>
-            </div>
-            </form>  
-        </article>
+<div id="mainCesta" class="row">
+    <section class="col-md-9 p-5" id="productosCesta">
         
-        <article class="col-md-12 pb-2"><form action="">
-            <span class="grande">Yoyo supersónico</span><br>
-            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
-            <div class="row">
-                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
-                <div class="font-weight-bold col-md-4 medio">Cantidad: 4</div>
-                <div class="font-weight-bold col-md-4 medio">Precio total: 3200€</div>
-            </div>
-            </form>  
-        </article>
-        
-        <article class="col-md-12 pb-2"><form action="">
-            <span class="grande">Yoyo supersónico</span><br>
-            <span class="medio">BARUM BARUM BARUM BARUM FLIPLI FLAPO FLIPI FLEN ESSESESESE JEJEJEJ TIRALE TIRALE TIRAAALEEEEE BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM BARUM</span><br>
-            <div class="row">
-                <div class="font-weight-bold col-md-4 medio">Precio: 800€</div>
-                <div class="font-weight-bold col-md-4 medio">Cantidad: 3</div>
-                <div class="font-weight-bold col-md-4 medio">Precio total: 2400€</div>
-            </div>
-            </form>  
-        </article>
     </section>
     
-    <section id="derecha" class="col-md-3">
+    <section class="col-md-3">
         <article class="m-5">
-            <table id="tabla">
+            <table cellpadding="15" id="tablaCesta">
                 <tr><td class="grande">Resumen de pedido</td></tr>
-                <tr><td>ss</td></tr>
-                <tr><td>ss</td></tr>
-                <tr><td>ss</td></tr>
-                <tr><td>ss</td></tr>
+                <tr><td class="font-weight-bold text-right medio">Productos: <span id="totalProductosCesta"></span>€</td></tr>
+                <tr><td class="font-weight-bold text-right medio">IVA: 21%</td></tr>
+                <tr><td class="font-weight-bold text-right medio">Gastos de envío: <span id="gastosDeEnvio">5.50</span>€</td></tr>
+                <tr><td class="text-right medio"></td></tr>
+                <tr><td class="text-right medio"></td></tr>
+                <tr><td class="text-right font-weight-bold medio">Total: <span id="totalCesta"></span>€</td></tr>
             </table>
         </article>
+        
+        <article class="ml-5">
+            <a href="pagar/"><img src="img/botonpago.png" class="ml-5" alt="Pagar"></a>
+        </article>
     </section>
-</main>
+</div>
 @endsection
