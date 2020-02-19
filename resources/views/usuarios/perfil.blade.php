0ecc864d50d8acab9016123668f48cd95b6f40ee
SandraAP1996 <34686149+SandraAP1996@users.noreply.github.com>
2020-02-19 12:57:35 +0100

    Modificación
    -CCS
    -UserController
    -perfil.blade

--- resources/views/usuarios/perfil.blade.php
+++ resources/views/usuarios/perfil.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.master')
 @section('contenido')
-<script src="/js/perfil-usuario.js"></script>
-<div id="usuario-perfil">
 
+@section('scripts')
+<script src="/js/perfil-usuario.js"></script>
+@endsection
+
+@section('titulo')
+    - Perfil
+@endsection
+
+
+
+
+<div id="usuario-perfil">
     <div id="contenedor-perfil">
         <h1>Perfil</h1>
         Nombre&nbsp;&nbsp;<span class="nombre" title="Doble click para cambiar">Pepa</span><br><br>
-        Apellidos&nbsp;&nbsp;<span class="apellido1" title="Doble click para cambiar">Rodrigo</span>&nbsp;<span class="apellido2" title="Doble click para cambiar">Morcillo</span> <br><br>
-        Fecha Nacimiento&nbsp;&nbsp; <span class="nacimiento" title="Doble click para cambiar">12-2-2018</span><br><br>
+        Apellidos&nbsp;&nbsp;<span id="apellido1" title="Doble click para cambiar">Rodrigo</span>&nbsp;<span id="apellido2" title="Doble click para cambiar">Morcillo</span> <br><br>
+        Fecha Nacimiento&nbsp;&nbsp; <span id="nacimiento" title="Doble click para cambiar">12-2-2018</span><br><br>
         <hr>
-        Email&nbsp;&nbsp;<span class="email" title="Doble click para cambiar">pepa@gmail.com</span><br><br>
+        Email&nbsp;&nbsp;<span id="email" title="Doble click para cambiar">pepa@gmail.com</span><br><br>
         <a href="#" id="recu-contras">Recuperar Contraseña</a>
 
     </div>
