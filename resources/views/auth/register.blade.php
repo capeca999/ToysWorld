1d7d4da7437bebd2c3d9d2c7675cbf5a5e4028bd
Capeca <capeca999@gmail.com>
2020-02-19 16:28:47 +0100

    Subida general
    19/02/2020

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,69 @@
                             </div>
                         </div>
 
+
+
+
+                        <div class="form-group row">
+                            <label for="nif" class="col-md-4 col-form-label text-md-right">{{ __('Nif') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nif" type="number" class="form-control @error('nif') is-invalid @enderror" name="nif" value="{{ old('nif') }}" required autocomplete="nif" autofocus>
+
+                                @error('nif')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="surname2" class="col-md-4 col-form-label text-md-right">{{ __('Surname2') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="surname2" type="text" class="form-control @error('surname2') is-invalid @enderror" name="surname2" value="{{ old('surname2') }}" required autocomplete="surname2" autofocus>
+
+                                @error('surname2')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="surname1" class="col-md-4 col-form-label text-md-right">{{ __('Surname1') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="surname1" type="text" class="form-control @error('surname1') is-invalid @enderror" name="surname1" value="{{ old('surname1') }}" required autocomplete="surname1" autofocus>
+
+                                @error('surname1')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="form-group row">
+                            <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Date_of_birth') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" autofocus>
+
+                                @error('date_of_birth')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
