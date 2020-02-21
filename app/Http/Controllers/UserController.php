084c49a9967c9010745e03b153f9817d1ed160b0
SandraAP1996 <34686149+SandraAP1996@users.noreply.github.com>
2020-02-21 02:54:28 +0100

    UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Auth;
 use App\User;
-
 use App\Order;
 use App\Line;
 use App\Product;
@@ -89,6 +89,8 @@ class UserController extends Controller
     {
         //
     }
+
+
     public static function perfilUsuario(){
         return view("usuarios.perfil");
     }
@@ -105,16 +107,18 @@ class UserController extends Controller
     }
 
     public static function historialUsuario(){
-        $historiales = User::select('lines.id_order','orders.payment_method','orders.total_price','products.price','orders.date','lines.quantity','products.name')
+
+        $historiales = User::select('lines.id_order','orders.payment_methods','orders.total_price','products.price','orders.date','lines.quantity','products.name')
             ->join('orders','users.nif','=','orders.nif')
             ->join('lines','orders.id','=','lines.id_order')
             ->join('products','lines.id_product','=','products.id')
-            ->where('orders.nif',Auth::user()->nif)
+            ->where('users.email',Auth::user()->email)
             ->get();
         return $historiales;
     }
+
     public static function historialAdmin($nif){
-        $historiales = User::select('','lines.id_order','orders.payment_method','orders.total_price','products.price','orders.date','lines.quantity','products.name')
+        $historiales = User::select('lines.id_order','orders.payment_method','orders.total_price','products.price','orders.date','lines.quantity','products.name')
             ->join('orders','users.nif','=','orders.nif')
             ->join('lines','orders.id','=','lines.id_order')
             ->join('products','lines.id_product','=','products.id')
@@ -146,16 +150,16 @@ class UserController extends Controller
     }
 
 
-
-    //    public static function historialUsuario($nif){
-    //        $historiales = User::select('line_product.id_order','orders.payment_method','orders.total_price','products.price','orders.date','line_product.quantity','products.name')
-    //            ->join('orders','users.nif','=','orders.nif')
-    //            ->join('line_product','orders.id','=','line_product.id_order')
-    //            ->join('products','line_product.id_product','=','products.id')
-    //            ->where('orders.nif',$nif)
-    //            ->get();
-    //        return $historiales;
-    //    }
+    /*
+    public static function historialUsuario(){
+        $historiales = User::select('line_product.id_order','orders.payment_method','orders.total_price','products.price','orders.date','line_product.quantity','products.name')
+            ->join('orders','users.nif','=','orders.nif')
+            ->join('line_product','orders.id','=','line_product.id_order')
+            ->join('products','line_product.id_product','=','products.id')
+            ->where('orders.nif',Auth::user()->nif)
+            ->get();
+        return $historiales;
+    }*/
 
     /*
     ->select('nif','name', 'surname1', 'surname2', 'email', 'date_of_birth')
